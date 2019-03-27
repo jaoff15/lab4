ff9a4b1db235d2d311af4605e057401fd38ad260
jaoff15 <jaoff15@student.sdu.dk>
2019-03-27 15:06:51 +0100

    First commit

---
+++ lab4.sdk/project/Debug/src/helloworld.d
@@ -0,0 +1,54 @@
+src/helloworld.o src/helloworld.o: ../src/helloworld.c ../src/platform.h \
+ ../src/platform_config.h \
+ ../../project_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../project_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../project_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../project_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../project_bsp/ps7_cortexa9_0/include/bspconfig.h \
+ ../../project_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../project_bsp/ps7_cortexa9_0/include/xbram.h \
+ ../../project_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../project_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../project_bsp/ps7_cortexa9_0/include/xbram_hw.h \
+ ../../project_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../project_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../project_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../project_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../project_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../project_bsp/ps7_cortexa9_0/include/xil_io.h
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+../../project_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../project_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../project_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../project_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../project_bsp/ps7_cortexa9_0/include/bspconfig.h:
+
+../../project_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../project_bsp/ps7_cortexa9_0/include/xbram.h:
+
+../../project_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../project_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../project_bsp/ps7_cortexa9_0/include/xbram_hw.h:
+
+../../project_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../project_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../project_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../project_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../project_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../project_bsp/ps7_cortexa9_0/include/xil_io.h:
